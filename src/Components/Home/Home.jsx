577523dbabb7efe146b927a2ed1de64d81bf794b
mehedi1802hasan <mehedi1802hasan@gmail.com>
2023-04-11 01:14:00 +0600

    added featured jobs parts

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 import Header from '../Header/Header';
+import Jobs from '../Jobs/Jobs';
 import JobCategoryList from '../JobCategoryList/JobCategoryList';
 import { useLoaderData } from 'react-router-dom';
 import './Home.css'
+
 const Home = () => {
     const jobcatagorylist=useLoaderData()
+    const [jobs,setJobs]=useState([])
+    useEffect(()=>{
+        fetch('featuredJobs.json')
+        .then(res=>res.json())
+        .then(data=>setJobs(data))
+    },[])
     return (
         <div>
            <Header></Header>
           <div >
-          <h1 className='jcl'>Job Category List</h1>
-            <p className='jcl'>Explore thousands of job opportunities with all the information you need. Its your future</p>
+          <h1 className='title'>Job Category List</h1>
+            <p className='sub title'>Explore thousands of job opportunities with all the information you need. Its your future</p>
           </div>
             <div className='grid grid-flow-row gap-10 m-20 my-20 m:grid-cols-1 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4'>    
             {
@@ -21,6 +29,19 @@ const Home = () => {
                 ></JobCategoryList>)
             }
             </div>
+            
+            <div >
+          <h1 className='title'>Featured Jobs</h1>
+            <p className='sub title'>Explore thousands of job opportunities with all the information you need. Its your future</p>
+          </div>
+          <div  className='grid grid-flow-row sm:gap-10 md:gap-20 sm:mx-7 md:mx-20 m:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2'>
+         {
+            jobs.map(job=> <Jobs
+                id={job.id}
+                job={job}
+            ></Jobs>)
+         }
+          </div>
         </div>
     );
 };
---
+++ src/Components/Jobs/Jobs.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Jobs = ({job}) => {
+    const {companyLogo,jobTitle,companyName,fullTime,location,salary,remote}=job
+    return (
+        <div>
+        <div className="shadow-xl bg-sky-50 card">
+                  
+                    <img className='mx-5' src={companyLogo}alt="Shoes"/>
+                    
+                    <div className=" card-body">
+                     <h2 className="card-title">{jobTitle}</h2>
+                     <p>{companyName}</p>
+                     <div >
+                        <button className="badge badge-outline">{remote}</button>
+                        <button className="badge badge-outline">{fullTime}</button>
+                     </div>
+                    <div className='flex gap-10'>
+                        <h6>{location}</h6> 
+                         <h6>salary: {salary}</h6>
+                    </div>
+                     <div className="card-actions">
+                         <button className="btn btn-primary">View Details</button>
+                     </div>
+                    </div>
+                
+           </div>
+       </div>
+    );
+};
+
+export default Jobs;
