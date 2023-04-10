46a514f593747acf209f9eba0350eb5c40f17a95
mehedi1802hasan <mehedi1802hasan@gmail.com>
2023-04-10 17:32:53 +0600

    added navbar & header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,40 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import './Header.css'
+
 const Header = () => {
     return (
-        <div className='header' >
-                <div className="mr-8 bg-yellow-100 -5 navbar">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <label tabIndex={0} className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-      </label>
-      
-    </div>
-   <h3 className='text-4xl text-bold '>JobBd</h3>
-  </div>
-  <div className="hidden navbar-center lg:flex">
-    <ul className="px-1 menu menu-horizontal">
-      <li> 
-           <Link to='/'>Home</Link>
-           </li>
-      <li tabIndex={0}>
-          <Link to='/statistics'>Statistics</Link>
-         </li>
-      <li> 
-          <Link to='/appliedjob'>Applied Jobs</Link>
-         </li>
-      <li> 
-          <Link to='/blog'>Blog</Link>
-        </li>
-    </ul>
-  </div>
-  <div className="navbar-end">
-  <Link className="btn" to='/getstarted'>Get started</Link>
-  </div>
-</div>
+             <div className='p-10 bg-yellow-100 md:flex'>
+             <div className="md:w-6/12 shadow-x card bg-base-100">
+             <div className="card-body">
+            <h1 className="card-title">One Step <br/> Closer To Your<br/>Dream Job </h1>
+           <p>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
+          <div >
+           <button className="btn btn-primary">Get Started</button>
+          </div>
+         </div>
         </div>
+        <div className='w-8/12'>
+            <img className='w-full h-96 ' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEhwReh1klkcvVymucw0ZdNfgCuVee5Eu55A&usqp=CAU' />
+        </div>
+        </div>
+    
     );
 };
 
---
+++ src/Components/JobCategoryList/JobCategoryList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const JobCategoryList = () => {
+    return (
+        <div>
+           <h2>HIii</h2>
+        </div>
+    );
+};
+
+export default JobCategoryList;
